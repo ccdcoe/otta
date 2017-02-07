1dc16905935509d2fb990164d416dbfcd4b2db98
Markus Kont <markuskont@gmail.com>
2017-02-07 11:09:47 +0200

    Bugfix

--- python/main.py
+++ python/main.py
@@ -57,7 +57,7 @@ QUERIES = [
         'aggregate_field': 'a1'
     },
     {
-        'expression': '(port.dst == 80) && protocols != http && vlan != 3611 && vlan != 3604 && (port.dst != 443 && protocols != tls) && tags != "acked-unseen-segment-dst" && tags != "out-of-order-src" && databytes != 0',
+        'expression': '(port.dst == 80 || port.dst == 8080 || port.dst == 9200 || port.dst == 8086) && protocols != http && vlan != 3611 && vlan != 3604 && (port.dst != 443 && protocols != tls) && tags != "acked-unseen-segment-dst" && tags != "out-of-order-src" && databytes != 0',
         'query_tag': 'not_http_std_port',
         'aggregate_field': 'a1'
     },
